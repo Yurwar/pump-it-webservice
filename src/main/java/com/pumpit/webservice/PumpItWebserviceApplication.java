36616a8a7bf489084695dd0698b122a009d26729
Yurii Matora <yura.matora11@gmail.com>
2020-06-01 01:47:38 +0300

    Initial commit

---
+++ src/main/java/com/pumpit/webservice/PumpItWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pumpit.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpItWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpItWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pumpit/webservice/PumpItWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pumpit.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpItWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
