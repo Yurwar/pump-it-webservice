80a26785b4c40dc5870e72b39ebc929d72808b53
Yurii Matora <yura.matora11@gmail.com>
2020-06-02 02:12:56 +0300

    Create trainer service

---
+++ src/main/java/com/pumpit/webservice/model/service/TrainerService.java
@@ -0,0 +1,14 @@
+package com.pumpit.webservice.model.service;
+
+import com.pumpit.webservice.model.entity.Client;
+import com.pumpit.webservice.model.entity.Trainer;
+
+import java.util.Set;
+
+public interface TrainerService {
+    Trainer getTrainerById(Long id);
+
+    Set<Client> getClientsForTrainerId(Long id);
+
+    void addClientForTrainerId(Long id, Client client);
+}
---
+++ src/main/java/com/pumpit/webservice/model/service/impl/DefaultTrainerService.java
@@ -0,0 +1,45 @@
+package com.pumpit.webservice.model.service.impl;
+
+import com.pumpit.webservice.model.entity.Client;
+import com.pumpit.webservice.model.entity.Trainer;
+import com.pumpit.webservice.model.repository.TrainerRepository;
+import com.pumpit.webservice.model.service.TrainerService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+import static org.springframework.transaction.annotation.Isolation.REPEATABLE_READ;
+
+@Service
+public class DefaultTrainerService implements TrainerService {
+    private final TrainerRepository trainerRepository;
+
+    public DefaultTrainerService(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
+    @Override
+    public Trainer getTrainerById(Long id) {
+        return trainerRepository.getOne(id);
+    }
+
+    @Override
+    @Transactional(isolation = REPEATABLE_READ)
+    public void addClientForTrainerId(Long trainerId, Client newClient) {
+        Trainer trainer = getTrainerById(trainerId);
+        addClientForTrainer(trainer, newClient);
+        trainerRepository.save(trainer);
+    }
+
+    @Override
+    public Set<Client> getClientsForTrainerId(Long id) {
+        return trainerRepository.getOne(id).getClients();
+    }
+
+    private void addClientForTrainer(Trainer trainer, Client newClient) {
+        Set<Client> clients = trainer.getClients();
+        clients.add(newClient);
+        trainer.setClients(clients);
+    }
+}
