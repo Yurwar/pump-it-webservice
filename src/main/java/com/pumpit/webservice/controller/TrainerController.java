76514229aae4ea4d915fb178fe676eceb12e00dd
Yurii Matora <yura.matora11@gmail.com>
2020-06-02 02:13:21 +0300

    Update Trainer Controller to work with trainer and his clients

--- src/main/java/com/pumpit/webservice/controller/TrainerController.java
+++ src/main/java/com/pumpit/webservice/controller/TrainerController.java
@@ -1,9 +1,34 @@
 package com.pumpit.webservice.controller;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import com.pumpit.webservice.model.entity.Client;
+import com.pumpit.webservice.model.entity.Trainer;
+import com.pumpit.webservice.model.service.TrainerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping("/trainers")
 public class TrainerController {
+    private final TrainerService trainerService;
+
+    public TrainerController(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
+
+    @GetMapping("/{id}")
+    public Trainer getTrainerById(@PathVariable Long id) {
+        return trainerService.getTrainerById(id);
+    }
+
+    @GetMapping("/{id}/clients")
+    public Set<Client> getClientsForTrainer(@PathVariable Long id) {
+        return trainerService.getClientsForTrainerId(id);
+    }
+
+    @PostMapping("/{id}/clients")
+    public void addClientForTrainer(@PathVariable Long id,
+                                    @RequestBody Client client) {
+        trainerService.addClientForTrainerId(id, client);
+    }
 }
